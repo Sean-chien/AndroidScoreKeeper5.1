369f068d3e306866b5645846fcfbd6e515cb86e6
Sea <bnly22335@gmail.com>
2019-05-14 17:23:07 -0700

    finish

---
+++ app/src/main/java/com/sean/chien/scorekeeper/MainActivity.java
@@ -0,0 +1,73 @@
+package com.sean.chien.scorekeeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mScore1;
+    private int mScore2;
+    private TextView mScoreText1;
+    private TextView mScoreText2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mScoreText1 = (TextView) findViewById(R.id.score_1);
+        mScoreText2 = (TextView) findViewById(R.id.score_2);
+    }
+
+    public void decreaseScore(View view) {
+        int viewID = view.getId();
+        switch (viewID) {
+            //If it was on Team 1
+            case R.id.decreaseTeam1:
+                //Decrement the score and update the TextView
+                mScore1--;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            //If it was Team 2
+            case R.id.decreaseTeam2:
+                //Decrement the score and update the TextView
+                mScore2--;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void increaseScore(View view) {
+        int viewID = view.getId();
+        switch (viewID) {
+            case R.id.increaseTeam1:
+                mScore1++;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+
+            case R.id.increaseTeam2:
+                mScore2++;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.night_mode){
+
+        }
+
+        return true;
+    }
+}
+
